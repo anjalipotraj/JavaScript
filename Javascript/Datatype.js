d8800f05a8b7493f6a6351c71abf513449844bee
anjalipotraj <anjalipotraj47@gmail.com>
2023-11-22 11:11:33 +0000

    basic

--- Javascript/Datatype.js
+++ Javascript/Datatype.js
@@ -3,3 +3,17 @@
 //alert(9+7)//we are using Nodejs, not browser
 
 console.log(5+5)
+
+/*Datatypes
+Number,STring,const,
+bigint,
+boolean
+null
+undefined
+symbol
+*/
+
+console.log(typeof"anjali");
+console.log(typeof null);
+console.log(typeof 43);
+console.log(typeof 3.7);
---
+++ Javascript/TypeConversion.js
@@ -0,0 +1,31 @@
+let score="33asd"
+
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let isLoggedIn=1
+let boolValue=Boolean(isLoggedIn)
+console.log(boolValue);
+
+//NON Primitive datatype
+//Array
+let heros=["shaktiman","Nagraj","Doga"]
+console.log(typeof heros);
+
+//Function
+const myfunction=function()
+{
+    console.log("Hello World");
+}
+
+console.log(typeof myfunction);
+
+//object
+let obj={
+    name:"anjali",
+    age:20,
+    city:"solapur"
+}
+console.log(typeof(obj))
