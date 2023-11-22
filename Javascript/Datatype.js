c3062b00cf12837a62c2ce5b0d4625e0a6fe89d8
anjalipotraj <anjalipotraj47@gmail.com>
2023-11-22 09:53:51 +0000

    test

---
+++ Javascript/Datatype.js
@@ -0,0 +1,5 @@
+"use strict"   //treat all code as newer version of js
+
+//alert(9+7)//we are using Nodejs, not browser
+
+console.log(5+5)
---
+++ Javascript/Variables.js
@@ -0,0 +1,16 @@
+const account_id=1234
+let name="Anjali"
+let email="anjalipotraj47@gmail.com"
+let ac_type="saving"
+city="Solapur"
+
+// account_id=5678  Not allow to chnage
+console.table([account_id,ac_type,name,email,city])
+ac_type="current"
+
+name="Pooja"
+console.log(account_id);
+console.log(name);
+console.log(email);
+console.log(ac_type);
+console.log(city);
